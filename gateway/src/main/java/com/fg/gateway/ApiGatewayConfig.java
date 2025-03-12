2591d0da41ebffa097956e571928989678896844
Tam Thong <120663808+tamthong1115@users.noreply.github.com>
2025-03-12 10:41:27 +0700

    gateway, config-server, discovery

---
+++ gateway/src/main/java/com/fg/gateway/ApiGatewayConfig.java
@@ -0,0 +1,16 @@
+package com.fg.gateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator gateRouter(RouteLocatorBuilder builder){
+        return builder.routes()
+                .build();
+    }
+}
