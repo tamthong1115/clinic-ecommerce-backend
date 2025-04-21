d738682602c6720d3c6b12887deada86930d5b8e
Le-Vu-Hoang <bx1536hoang@gmail.com>
2025-04-21 17:17:44 +0700

    config clinic in gateway + role

--- clinic-service/src/main/java/com/fg/clinicservice/config/feign/AuthClient.java
+++ clinic-service/src/main/java/com/fg/clinicservice/config/feign/AuthClient.java
@@ -2,11 +2,13 @@ package com.fg.clinicservice.config.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
 @FeignClient(name = "auth-service")
 public interface AuthClient {
     @GetMapping("api/v1/me")
-    ResponseEntity<UserDto> getCurrentUser();
+    ResponseEntity<UserDto> getCurrentUser(@AuthenticationPrincipal UserDetails userDetails);
 }
